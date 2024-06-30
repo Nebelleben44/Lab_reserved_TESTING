5f0de2c8d2e3040d043f76d84e15d92d175313d4
Nebelleben44 <yanawat.pa@ku.th>
2024-06-30 11:34:07 +0700

    Update app_V2.py

--- app_V2.py
+++ app_V2.py
@@ -954,7 +954,7 @@ else:
         }
     }
 
-    st.write(st.secrets["credentials"]["yanawat4511"])
+    st.write(st.secrets["credentials"]["usernames"]['yanawat4511'])
 
     # Function to authenticate users
     def authenticate(username, password):
